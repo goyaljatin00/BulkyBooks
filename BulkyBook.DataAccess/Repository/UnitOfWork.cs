ff955182880d6b7dca1b30ea17c5f7d2ff2c6d65
goyaljatin00 <62867300+goyaljatin00@users.noreply.github.com>
2021-05-07 12:09:34 +0530

    Section 5

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ICategoryRepository Category { get; private set; }
+
+        public ISP_CALL SP_CALL { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_CALL = new SP_Call(_db);
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
